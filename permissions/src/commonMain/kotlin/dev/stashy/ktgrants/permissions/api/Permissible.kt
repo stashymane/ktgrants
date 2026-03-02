6ab983c8bfdea4de1840ccad5a3045dcd5bbc169
stashymane <me@stashy.dev>
2026-03-02 19:56:16 +0200

    make delegate classes private

--- permissions/src/commonMain/kotlin/dev/stashy/ktgrants/permissions/api/Permissible.kt
+++ permissions/src/commonMain/kotlin/dev/stashy/ktgrants/permissions/api/Permissible.kt
@@ -8,26 +8,34 @@ public sealed interface Permissible {
         public val group: Group
         public val subject: Subject
 
-        public data class Snapshot(override val group: Group, override val subject: Subject) : Entity
+        private data class Snapshot(override val group: Group, override val subject: Subject) : Entity
+
+        public companion object {
+            public operator fun invoke(group: Group, subject: Subject): Entity = Snapshot(group, subject)
+        }
     }
 
     public interface Type : Permissible {
         public val group: Group
 
-        public fun withSubject(subject: Subject): Entity = Entity.Snapshot(group, subject)
+        public fun withSubject(subject: Subject): Entity = Entity(group, subject)
         public fun withSubject(subject: Subject.Provider): Entity = withSubject(subject.toSubject())
         public fun withSubject(subject: String): Entity = withSubject(Subject(subject))
 
-        public data class Snapshot(override val group: Group) : Type
+        private data class Snapshot(override val group: Group) : Type
+
+        public companion object {
+            public operator fun invoke(group: Group): Type = Snapshot(group)
+        }
     }
 
     public companion object {
-        public operator fun invoke(group: Group, subject: Subject): Entity = Entity.Snapshot(group, subject)
-        public operator fun invoke(group: Group): Type = Type.Snapshot(group)
+        public operator fun invoke(group: Group, subject: Subject): Entity = Entity(group, subject)
+        public operator fun invoke(group: Group): Type = Type(group)
 
         public inline operator fun <reified T> invoke(subjectProvider: Subject.Provider): Entity =
             invoke(Group(T::class), subjectProvider.toSubject())
-
+        
         public inline operator fun <reified T> invoke(): Type =
             invoke(Group(T::class))
     }
