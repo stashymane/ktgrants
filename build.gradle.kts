d4973cc0c2321db70f9ca4bb95656091bba214a5
stashymane <me@stashy.dev>
2026-03-02 20:15:06 +0200

    add dokka plugin

--- build.gradle.kts
+++ build.gradle.kts
@@ -4,4 +4,13 @@ plugins {
     alias(libs.plugins.kotlin.multiplatform) apply false
     alias(libs.plugins.kotlin.serialization) apply false
     alias(libs.plugins.ksp) apply false
+    alias(libs.plugins.dokka)
+}
+
+dependencies {
+    dokka(projects.permissions)
+}
+
+dokka {
+
 }
