b60a969c1a3b7cd73f19150757003d4993cba694
stashymane <me@stashy.dev>
2026-06-30 14:54:17 +0300

    provide GrantDsl from PermissionConfig

--- permissions/src/commonMain/kotlin/dev/stashy/ktgrants/permissions/api/PermissionConfig.kt
+++ permissions/src/commonMain/kotlin/dev/stashy/ktgrants/permissions/api/PermissionConfig.kt
@@ -3,8 +3,9 @@ package dev.stashy.ktgrants.permissions.api
 import dev.stashy.ktgrants.permissions.Permission
 import dev.stashy.ktgrants.permissions.PermissionResolver
 import dev.stashy.ktgrants.permissions.SubjectProvider
+import dev.stashy.ktgrants.permissions.api.dsl.GrantDsl
 
-public interface PermissionConfig : GrantModel {
+public interface PermissionConfig : GrantModel, GrantDsl {
     public val resolver: PermissionResolver
 
     public fun actor(subject: SubjectProvider, permissions: Set<Permission>): Actor =
